2d50a9196d5ce59d3c1f3fdf95f3444ecb842870
Yan Yang <ssyangy@Yan-Yangs-MacBook-Air.local>
2011-04-12 23:21:47 +0800

    change project.desc from string to text & remove project admin method

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110411123637) do
+ActiveRecord::Schema.define(:version => 20110412145417) do
 
   create_table "members", :force => true do |t|
     t.integer  "project_id"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(:version => 20110411123637) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
-    t.string   "desc"
+    t.text     "desc",       :limit => 255
     t.integer  "owner"
     t.datetime "created_at"
     t.datetime "updated_at"
